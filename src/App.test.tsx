6e865b2d0d0930015096648c45c4aaf0f7b9a280
matborowiak <m.borowiak@hotmail.com>
2021-01-14 22:08:56 +0100

    refactor App component, tests, styles

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders the logo', () => {
+  render(<App />)
+  const logo = screen.getByText(/Billie.io/i)
+  expect(logo).toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import CustomerList from './components/CustomerList'
+import Header from './components/Header'
 
-export default App;
+import './App.scss'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+const App = () => (
+  <div id="main" className="App">
+    <Header />
+    <QueryClientProvider client={queryClient}>
+      <CustomerList />
+    </QueryClientProvider>
+  </div>
+)
+
+export default App
